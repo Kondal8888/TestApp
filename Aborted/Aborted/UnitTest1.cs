43b92e3a8ea5d30ace9bcf6d4dc7f7d1bf11ae42
Kondal8888 <43537421+Kondal8888@users.noreply.github.com>
2019-05-30 16:04:13 +0530

    Update UnitTest1.cs

--- Aborted/Aborted/UnitTest1.cs
+++ Aborted/Aborted/UnitTest1.cs
@@ -1,36 +1,40 @@
-﻿using System;
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Aborted
 {
     [TestClass]
-    public class UnitTest1
+    public class SomeTestClass
     {
+        Thread testThread;
+
+        [TestInitialize]
+        public void BeforeTest()
+        {
+            testThread = Thread.CurrentThread;
+            var task = new Task(abortIfTestStilRunsAfterTimeout);
+            task.Start();
+        }
+
         [TestMethod]
-        public void TestMethod1()
+        public void TestMethod()
         {
-            Assert.IsFalse(false);
+            try
+            {
+                Thread.Sleep(5000);
+            }
+            catch (ThreadAbortException e)
+            {
+                Assert.Fail((string)e.ExceptionState);
+            }
         }
-        public void TestMethod2()
+
+        private void abortIfTestStilRunsAfterTimeout()
         {
-            Assert.IsFalse(true);
-        }
-  
-        public void TestMethod3()
-        {
-            Assert.IsFalse(false);
-        }
-        public void TestMethod4()
-        {
-            Assert.IsFalse(true);
-        }
-        public void TestMethod5()
-        {
-            Assert.IsFalse(false);
-        }
-        public void TestMethod6()
-        {
-            Assert.IsFalse(false);
+            testThread.Abort("timeout passed!");
         }
     }
 }
