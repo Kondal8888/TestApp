858c89c2ae4378b838f87c0ec2b9119d88f30c35
Kondal8888 <43537421+Kondal8888@users.noreply.github.com>
2019-06-20 14:30:21 +0530

    updated

--- Aborted/Aborted/UnitTest1.cs
+++ Aborted/Aborted/UnitTest1.cs
@@ -9,32 +9,43 @@ namespace Aborted
     [TestClass]
     public class SomeTestClass
     {
-        Thread testThread;
+        //Thread testThread;
 
-        [TestInitialize]
-        public void BeforeTest()
-        {
-            testThread = Thread.CurrentThread;
-            var task = new Task(abortIfTestStilRunsAfterTimeout);
-            task.Start();
-        }
+        //[TestInitialize]
+        //public void BeforeTest()
+        //{
+        //    testThread = Thread.CurrentThread;
+        //    var task = new Task(abortIfTestStilRunsAfterTimeout);
+        //    task.Start();
+        //}
 
+        //[TestMethod]
+        //public void TestMethod()
+        //{
+        //    try
+        //    {
+        //        Thread.Sleep(5000);
+        //    }
+        //    catch (ThreadAbortException e)
+        //    {
+        //        Assert.Fail((string)e.ExceptionState);
+        //    }
+        //}
+
+        //private void abortIfTestStilRunsAfterTimeout()
+        //{
+        //    testThread.Abort("timeout passed!");
+        //}
         [TestMethod]
-        public void TestMethod()
+        public void TestMethod1()
         {
-            try
-            {
-                Thread.Sleep(5000);
-            }
-            catch (ThreadAbortException e)
-            {
-                Assert.Fail((string)e.ExceptionState);
-            }
+            foo();
         }
 
-        private void abortIfTestStilRunsAfterTimeout()
+
+        public void foo()
         {
-            testThread.Abort("timeout passed!");
+            foo();
         }
     }
 }
